48be94ddfb95b0b0f0f719f5c53236ee6b2d0c28
Eric Taylor <eric@daxos.com>
2013-05-27 02:18:35 -0600

    Changed production mailer to www.elusiveindex.com. Test Heroku launch.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,8 @@ ElusiveIndexInvite::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'elusiveindexinvite.herokuapp.com' }
-  # config.action_mailer.default_url_options = { :host => 'www.elusiveindex.com' }
+  # config.action_mailer.default_url_options = { :host => 'elusiveindexinvite.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'www.elusiveindex.com' }
 
   # Send emails via AWS SES
   config.action_mailer.delivery_method = :smtp
