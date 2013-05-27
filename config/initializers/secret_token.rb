4b7277575b8036be7708b705e436bf77970f64ef
Eric Taylor <eric@daxos.com>
2013-05-27 00:49:33 -0600

    Init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElusiveIndexInvite::Application.config.secret_token = 'f8c73c29b9b8d0ee9b58d11c783d606b0034446333b651ea3639bbce46b9e13d220c5e508dd83a705d792112ed2f1570e9d939bf785f27f96139234b586689d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ElusiveIndexInvite::Application.config.session_store :cookie_store, key: '_ElusiveIndexInvite_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ElusiveIndexInvite::Application.config.session_store :active_record_store
