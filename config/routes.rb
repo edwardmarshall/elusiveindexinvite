2214b56675bae1cd7b2a248d39dcda1022427317
Eric Taylor <eric@daxos.com>
2013-05-27 01:24:00 -0600

    Added custom confirmations and registrations controller. Setup new routes for devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 ElusiveIndexInvite::Application.routes.draw do
   root :to => "home#index"
-  
-  devise_for :users
+
+  devise_for  :users,
+              :path => "invite",
+              :controllers => { :registrations => "registrations",
+                                :confirmations => "confirmations",
+                                :sessions => "devise/sessions",
+                              },
+              :skip => [:sessions] do 
+                get   "/login" => "devise/sessions#new", :as => :new_user_session
+                post  "/login" => "devise/sessions#create", :as => :user_session
+                get   "/logout" => "devise/sessions#destroy", :as => :destroy_user_session
+                get   "/register" => "registrations#new", :as => :new_user_registration
+              end
+
+  devise_scope :user do
+    # Matches for Devise here.
+  end
 end
